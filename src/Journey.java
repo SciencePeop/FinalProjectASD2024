5d83acb5764874d29b6ebdba33242152bf412618
Hizkia Crisantino <hizkiacrisantino98@gmail.com>
2024-12-09 23:42:18 +0700

    - Mengubah currentMilestone dan nextMilestone - Memperbaiki alur kehabisan energi - Membuat getMinCostOfNextStep

--- src/Journey.java
+++ src/Journey.java
@@ -6,23 +6,29 @@ public class Journey {
     private List<Milestone> milestoneList;
     Scanner scanner = new Scanner(System.in);
 
-    public Journey(Potter potter, Monster monster){
+    public Journey(Potter potter, Monster monster) {
         this.potter = potter;
         this.monster = monster;
         this.milestoneList = initialization();
     }
 
-    public void startGame(Milestone startChoosen){
-        System.out.println("Starting the game...");
-        Milestone currentMilestone = milestoneList.get(0);
-        Milestone nextMilestone = startChoosen;
-        List<Milestone> optionMilestone;
 
-        while (true){
+    public void startGame(String startId) {
+        System.out.println("\nStarting the game...");
+
+        Milestone currentMilestone = milestoneList.stream()
+                .filter(milestone -> milestone.getId().equals(startId))
+                .findFirst().orElse(null);
+        Milestone nextMilestone = null;
+        List<Milestone> optionMilestone;
+        String[] mainQuestVisited = {"mQ1", "mQ2", "mQ3", "mQ4", "mQ5"};
+
+        while (true) {
+            System.out.println("\nYou choose " + currentMilestone.getName());
             potter.printStatus();
 
-            //Kondisi jika tidak memiliki energi yang cukup
-            if(potter.getEnergy()<currentMilestone.getEnergyCosts(nextMilestone)){
+            //Kondisi jika tidak memiliki energi yang cukup untuk melanjutkan satupun quest yang ada
+            if (potter.getEnergy() < getMinCostOfNextStep(currentMilestone)) {
                 System.out.println("Your energy is not enough to continue the journey. You lose.");
                 break;
             }
@@ -34,42 +40,54 @@ public class Journey {
             // Tampilkan opsi milestone
             System.out.println("Available paths:");
             for (int i = 0; i < optionMilestone.size(); i++) {
-                System.out.println(i+1 + ". " + optionMilestone.get(i).getName());
-                System.out.println("Cost : "+ optionMilestone.get(i).getEnergyCosts(currentMilestone)+
-                        "\t Coins : "+optionMilestone.get(i).getCoinReward()+
-                        "\t Energy : "+optionMilestone.get(i).getEnergyRestored());
+                System.out.println(i+1 +". "+ optionMilestone.get(i).getId() + " : " + optionMilestone.get(i).getName());
+                System.out.println(optionMilestone.get(i).getDescription());
+                System.out.println("Cost : " + optionMilestone.get(i).getEnergyCosts(currentMilestone)
+                        + " \tCoins : "+optionMilestone.get(i).getCoinReward()
+                        +" \tEnergy : "+ optionMilestone.get(i).getEnergyRestored());
             }
-            System.out.println(optionMilestone.size()+1+". Use Pusaka");
+            System.out.println(optionMilestone.size() + 1 + ". Use Pusaka");
+            System.out.println("Cost : " + potter.getEnergy() * 0.6);
 
             // Ambil pilihan pengguna
             boolean validChoice = false;
             int choice = 1;
             while (!validChoice) {
-                System.out.print("Choose your next milestone by index: ");
+                System.out.print("Choose your next milestone: ");
                 choice = scanner.nextInt();
 
                 // Validasi pilihan
-                if (choice < 0 || choice > optionMilestone.size()+1) {
+                if (choice < 0 || choice > optionMilestone.size() + 1) {
                     System.out.println("Invalid choice. Please try again.");
                     continue;
                 }
                 validChoice = true;
             }
 
+            //Kondisi jika nextMilestone adalah finish
 
             //Kondisi jika menggunakan Pusaka
-            if (choice==optionMilestone.size()+1) {
+            if (choice == optionMilestone.size() + 1) {
                 System.out.println("Using Pusaka");
                 break;
             }
             // Perbarui milestone berikutnya
-            nextMilestone = optionMilestone.get(choice-1);
+            nextMilestone = optionMilestone.get(choice - 1);
 
             // Update potter's energy dan milestone saat ini
             potter.decreaseEnergy(currentMilestone.getEnergyCosts(nextMilestone));
             potter.addCoins(currentMilestone.getCoinReward());
             potter.increaseEnergy(currentMilestone.getEnergyRestored());
-            currentMilestone = nextMilestone;
+            currentMilestone.setCompleted();
+
+            //Hapus jika mainQuest
+            for (int i = 0; i < mainQuestVisited.length; i++) {
+                if (mainQuestVisited[i].equals(currentMilestone.getId())) {
+                    mainQuestVisited[i] = null; // Ubah nilai menjadi null
+                    break; // Keluar dari loop setelah menemukan
+                }
+                currentMilestone = nextMilestone;
+            }
         }
     }
 
@@ -80,39 +98,45 @@ public class Journey {
         return new ArrayList<>(currentMilestone.getConnectedMilestones().keySet());
     }
 
-    public static List<Milestone> initialization(){
+    public static int getMinCostOfNextStep(Milestone currentMilestone) {
+        ArrayList<Integer> costs = new ArrayList<>(currentMilestone.getConnectedMilestones().values());
+        int minimumCost = Collections.min(costs);
+        return minimumCost;
+    }
+
+    private List<Milestone> initialization() {
         List<Milestone> milestoneList = new ArrayList<>();
-        Milestone start = new Milestone("Naradhista Castle", "Start");
+        Milestone start = new Milestone("s", "Naradhista Castle", "Start");
 
-        Milestone start1 = new Milestone("Field of Light", "Start Path");
-        Milestone start2 = new Milestone("Fortress of Faith", "Start Path");
-        Milestone start3 = new Milestone("Tower of Fate’s Dawn", "Start Path");
+        Milestone start1 = new Milestone("s1", "Field of Light", "Start Path");
+        Milestone start2 = new Milestone("s2", "Fortress of Faith", "Start Path");
+        Milestone start3 = new Milestone("s3", "Tower of Fate’s Dawn", "Start Path");
 
-        Milestone mQ1 = new Milestone("Hurricane Shoes", "Main Quest");
-        Milestone mQ2 = new Milestone("Taranis Shield", "Main Quest");
-        Milestone mQ3 = new Milestone("Phoenix Mantle", "Main Quest");
-        Milestone mQ4 = new Milestone("Gandiva Arrow", "Main Quest");
-        Milestone mQ5 = new Milestone("Helm of Kuuga", "Main Quest");
+        Milestone mQ1 = new Milestone("mQ1", "Hurricane Shoes", "Main Quest");
+        Milestone mQ2 = new Milestone("mQ2", "Taranis Shield", "Main Quest");
+        Milestone mQ3 = new Milestone("mQ3", "Phoenix Mantle", "Main Quest");
+        Milestone mQ4 = new Milestone("mQ4", "Gandiva Arrow", "Main Quest");
+        Milestone mQ5 = new Milestone("mQ5", "Helm of Kuuga", "Main Quest");
 
-        Milestone sQ1 = new Milestone("Gather Food Supplies", 0, 17);
-        Milestone sQ2 = new Milestone("Repair the Broken Cart", 0, 5);
-        Milestone sQ3 = new Milestone("Assist the Villagers", 13, 5);
-        Milestone sQ4 = new Milestone("Upgrade Village Walls", 0, 8);
-        Milestone sQ5 = new Milestone("Scout the Nearby Forest", 0, 5);
-        Milestone sQ6 = new Milestone("Find the Lost Tools", 12, 5);
-        Milestone sQ7 = new Milestone("Build the Watchtower", 15, 0);
-        Milestone sQ8 = new Milestone("Defend Against Bandits", 0, 5);
-        Milestone sQ9 = new Milestone("Rescue the Stranded Traveler", 14, 11);
-        Milestone sQ10 = new Milestone("Trade Goods with the Caravan", 0, 7);
-        Milestone sQ11 = new Milestone("Locate the Hidden Spring", 15, 10);
-        Milestone sQ12 = new Milestone("Train the Village Guard", 22, 0);
-        Milestone sQ13 = new Milestone("Collect Medicinal Herbs", 0, 7);
-        Milestone sQ14 = new Milestone("Repel the Night Raid", 0, 5);
-        Milestone sQ15 = new Milestone("Retrieve the Ancient Relic", 17, 15);
+        Milestone sQ1 = new Milestone("sQ1", "Gather Food Supplies", 0, 17);
+        Milestone sQ2 = new Milestone("sQ2", "Repair the Broken Cart", 0, 5);
+        Milestone sQ3 = new Milestone("sQ3", "Assist the Villagers", 13, 5);
+        Milestone sQ4 = new Milestone("sQ4", "Upgrade Village Walls", 0, 8);
+        Milestone sQ5 = new Milestone("sQ5", "Scout the Nearby Forest", 0, 5);
+        Milestone sQ6 = new Milestone("sQ6", "Find the Lost Tools", 12, 5);
+        Milestone sQ7 = new Milestone("sQ7", "Build the Watchtower", 15, 0);
+        Milestone sQ8 = new Milestone("sQ8", "Defend Against Bandits", 0, 5);
+        Milestone sQ9 = new Milestone("sQ9", "Rescue the Stranded Traveler", 14, 11);
+        Milestone sQ10 = new Milestone("sQ10", "Trade Goods with the Caravan", 0, 7);
+        Milestone sQ11 = new Milestone("sQ11", "Locate the Hidden Spring", 15, 10);
+        Milestone sQ12 = new Milestone("sQ12", "Train the Village Guard", 22, 0);
+        Milestone sQ13 = new Milestone("sQ13", "Collect Medicinal Herbs", 0, 7);
+        Milestone sQ14 = new Milestone("sQ14", "Repel the Night Raid", 0, 5);
+        Milestone sQ15 = new Milestone("sQ15", "Retrieve the Ancient Relic", 17, 15);
 
-        Milestone f1 = new Milestone("Infernal Dragon");
-        Milestone f2 = new Milestone("Dark Leviathan");
-        Milestone f3 = new Milestone("Blood Wraith");
+        Milestone f1 = new Milestone("f1", "Infernal Dragon", "Final");
+        Milestone f2 = new Milestone("f2", "Dark Leviathan", "Final");
+        Milestone f3 = new Milestone("f3", "Blood Wraith", "Final");
 
         start.addConnection(start1, 0);
         start.addConnection(start2, 0);
@@ -257,6 +281,7 @@ public class Journey {
         milestoneList.add(f1);
         milestoneList.add(f2);
         milestoneList.add(f3);
+
         return milestoneList;
     }
 }
